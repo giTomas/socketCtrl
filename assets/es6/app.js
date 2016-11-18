ef8d3cb54d8026efcbf5384fa5997021f3326715
Kosegi <SK040122@main.intgin.net>
2016-11-18 16:49:52 +0100

    cmd

---
+++ assets/es6/app.js
@@ -0,0 +1,58 @@
+const pass = '&pass=config';
+const event = '/event?port=';
+
+const parts = {
+  pass: '&pass=config',
+  event: '/event?port='
+}
+
+let sockets = [false, 'u', true, 'u'];
+
+let socket1 = false;
+let socket2 = 'u';
+let socket3 = false;
+let socket4 = 'u';
+
+const pipe = (...fns) => (value) => fns.reduce((v, fn) => fn(v), value);
+
+const makeString = socket =>
+                  (string='') =>
+                    typeof socket === 'string' ?
+                    string + socket : socket ?
+                    string + '1' : string + '0';
+
+const makeString2 = sockets.map(socket =>
+                                  typeof socket === 'string' ?
+                                  string + socket : socket ?
+                                  string + '1' : string + '0'
+).join('');
+
+console.log(makeString2)
+
+const composeUrl = ({pass, event}) =>
+                    string => event + string + pass;
+
+
+
+const socketState1 = makeString(socket1);
+const socketState2 = makeString(socket2);
+const socketState3 = makeString(socket3);
+const socketState4 = makeString(socket4);
+const compose = composeUrl(parts);
+
+const command = pipe(socketState1, socketState2, socketState3, socketState4, compose);
+
+const url = command();
+
+console.log(url);
+
+//const completeUrl = () => event + '1uuu' + pass;
+
+// console.log(completeUrl());
+
+//const url = completeUrl()
+
+axios({method: 'get',
+       url: url,
+       baseURL: 'http://172.28.182.248',
+       withCredentials: true});
---
+++ build/js/app.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var pass = '&pass=config';
+var event = '/event?port=';
+
+var parts = {
+  pass: '&pass=config',
+  event: '/event?port='
+};
+
+var sockets = [false, 'u', true, 'u'];
+
+var socket1 = false;
+var socket2 = 'u';
+var socket3 = false;
+var socket4 = 'u';
+
+var pipe = function pipe() {
+  for (var _len = arguments.length, fns = Array(_len), _key = 0; _key < _len; _key++) {
+    fns[_key] = arguments[_key];
+  }
+
+  return function (value) {
+    return fns.reduce(function (v, fn) {
+      return fn(v);
+    }, value);
+  };
+};
+
+var makeString = function makeString(socket) {
+  return function () {
+    var string = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+    return typeof socket === 'string' ? string + socket : socket ? string + '1' : string + '0';
+  };
+};
+
+var composeUrl = function composeUrl(_ref) {
+  var pass = _ref.pass,
+      event = _ref.event;
+  return function (string) {
+    return event + string + pass;
+  };
+};
+
+var socketState1 = makeString(socket1);
+var socketState2 = makeString(socket2);
+var socketState3 = makeString(socket3);
+var socketState4 = makeString(socket4);
+var compose = composeUrl(parts);
+
+var command = pipe(socketState1, socketState2, socketState3, socketState4, compose);
+
+var url = command();
+
+console.log(url);
+
+//const completeUrl = () => event + '1uuu' + pass;
+
+// console.log(completeUrl());
+
+//const url = completeUrl()
+
+axios({ method: 'get',
+  url: url,
+  baseURL: 'http://172.28.182.248',
+  withCredentials: true });
